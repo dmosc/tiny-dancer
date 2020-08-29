73686f5fa1a1e96b547655baa310acdb90c0487a
Oscar David Rodríguez Moscosa <oscardavidrodriguezm@gmail.com>
2020-08-29 13:59:29 -0500

    setup (initial setup) Add initial folder setup for project.
    + Added `app/` folder to contain frontend service.
    + Added `server/` folder to contain backend service.

---
+++ app/src/views/main/components/sign-document/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Card} from 'antd';
+
+const SignDocument = () => {
+  return (
+      <Card title="Document Signature" bordered={false}>
+        View to sign documents
+      </Card>
+  );
+};
+
+export default SignDocument;
