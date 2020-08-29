d5e949d07b42f37f75184ea9dec35cea7094988c
Pamela Lozano <pamelalozano16@gmail.com>
2020-08-29 18:42:12 -0500

    post register

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,23 +1,39 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 // Login user
 
-export const login = async (email, password) => {
-  console.log(email, password);
-  //   const config = {
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   };
-  //   const body = JSON.stringify({ email, password });
-  //   try {
-  //     const res = await axios.post("/api/auth", body, config);
-
-  //   } catch (err) {
-  //     const errors = err.response.data.errors;
-  //     if (errors) {
-  //       errors.forEach((error) => console.log(error));
-  //     }
-
-  //   }
+export const login = async (
+  email,
+  firstName,
+  lastName,
+  username,
+  image,
+  account,
+  signature,
+) => {
+  console.log(email, firstName, lastName, username, account, signature);
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const body = JSON.stringify({
+    email,
+    firstName,
+    lastName,
+    username,
+    image,
+    account,
+    signature,
+  });
+  try {
+    const res = await axios.post('/user/register', body, config);
+    console.log(res);
+    localStorage.setItem('token', res.token);
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => console.log(error));
+    }
+  }
 };
