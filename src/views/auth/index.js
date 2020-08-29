0eb0d293b2540b0b49ecddcd6884ed4db226b1bf
jessica garcia <jessicagarcia@jessicas-mbp.lan>
2020-08-29 15:34:46 -0500

    Pagina login

--- src/views/auth/index.js
+++ src/views/auth/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import {Card} from 'antd';
 
 const Auth = () => {
   return (
-    <Card title="Login/Register" bordered={false}>
-      View to handle login/register forms
-    </Card>
+    <Fragment>
+      {' '}
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign In Your Account
+      </p>
+      <form className="form">
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            required
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">Dont have an account? Sign Up</p>
+    </Fragment>
   );
 };
 
