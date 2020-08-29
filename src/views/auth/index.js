153a66fa73b191b36ce4de7f952e59d9c2f80467
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2020-08-29 18:10:02 -0500

    Unstyled form

--- src/views/auth/index.js
+++ src/views/auth/index.js
@@ -1,13 +1,16 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import {login} from '../../actions/auth';
 import Web3 from 'web3';
+import {Link} from 'react-router-dom';
 
 const Auth = () => {
   const [formData, setFormData] = useState({
     email: '',
-    password: '',
+    firstName: '',
+    lastName: '',
+    username: '',
   });
-  const {email, password} = formData;
+  const {email, firstName, lastName, username} = formData;
 
   const onChange = (e) =>
     setFormData({...formData, [e.target.name]: e.target.value});
@@ -29,7 +32,7 @@ const Auth = () => {
       );
 
       console.log(signature);
-      login(email, password);
+      login(email, firstName, lastName, username, '', accounts[0], signature);
     } else {
       needMetamaskAlert();
     }
@@ -68,18 +71,39 @@ const Auth = () => {
         </div>
         <div className="form-group">
           <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            minLength="6"
-            value={password}
+            type="firstName"
+            placeholder="First Name"
+            name="firstName"
+            value={firstName}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="lastName"
+            placeholder="Last Name"
+            name="lastName"
+            value={lastName}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="username"
+            placeholder="username"
+            name="username"
+            value={username}
             onChange={(e) => onChange(e)}
             required
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Login" />
       </form>
-      <p className="my-1">Dont have an account? Sign Up</p>
+      <p className="my-1">
+        Dont have an account? <Link to="/login">Sign Up</Link>
+      </p>
     </Fragment>
   );
 };
