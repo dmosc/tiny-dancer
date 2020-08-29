ecdd0cef29542eb93f463a22efba1e1f823aef68
Pamela Lozano <pamelalozano16@gmail.com>
2020-08-29 17:05:41 -0500

    Open metamask login and sign
    nit
    
    nit
    
    nit
    
    nit

---
+++ src/actions/auth.js
@@ -0,0 +1,23 @@
+// import axios from 'axios';
+
+// Login user
+
+export const login = async (email, password) => {
+  console.log(email, password);
+  //   const config = {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   };
+  //   const body = JSON.stringify({ email, password });
+  //   try {
+  //     const res = await axios.post("/api/auth", body, config);
+
+  //   } catch (err) {
+  //     const errors = err.response.data.errors;
+  //     if (errors) {
+  //       errors.forEach((error) => console.log(error));
+  //     }
+
+  //   }
+};
--- src/views/auth/index.js
+++ src/views/auth/index.js
@@ -1,6 +1,48 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
+import {login} from '../../actions/auth';
+import Web3 from 'web3';
 
 const Auth = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const {email, password} = formData;
+
+  const onChange = (e) =>
+    setFormData({...formData, [e.target.name]: e.target.value});
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  useEffect(() => {
+    openSignIn();
+  });
+  /**
+   * Opens Metamask sign in
+   */
+  async function openSignIn() {
+    if (!window.web3) {
+      // If not injected web3 environment, show a warning
+      alert('This app needs some web3 provider such as metamask');
+    } else {
+      // Instantiate a new web3 with full capabilities
+      const web3 = new Web3(Web3.givenProvider, null, {});
+
+      // Request the user account and save it into input
+      const accounts = await web3.eth.requestAccounts();
+      console.log(accounts);
+      const signature = await web3.eth.personal.sign(
+        'Please sign here',
+        accounts[0],
+        '',
+      );
+      console.log(signature);
+    }
+  }
+
   return (
     <Fragment>
       {' '}
@@ -8,12 +50,14 @@ const Auth = () => {
       <p className="lead">
         <i className="fas fa-user"></i> Sign In Your Account
       </p>
-      <form className="form">
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
         <div className="form-group">
           <input
             type="email"
             placeholder="Email Address"
             name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
             required
           />
         </div>
@@ -23,10 +67,11 @@ const Auth = () => {
             placeholder="Password"
             name="password"
             minLength="6"
+            value={password}
+            onChange={(e) => onChange(e)}
             required
           />
         </div>
-
         <input type="submit" className="btn btn-primary" value="Login" />
       </form>
       <p className="my-1">Dont have an account? Sign Up</p>
