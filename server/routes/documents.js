92f538a88bf10f10bdc618ea6af2ab9ec64fecdb
oscardavidrm <oscardavidrodriguezm@gmail.com>
2020-08-30 04:29:22 -0500

    bugfix (import) Remove unnecessary import.

--- server/routes/documents.js
+++ server/routes/documents.js
@@ -2,7 +2,6 @@ import {Router} from 'express';
 import jwt from 'jsonwebtoken';
 import {User, Document} from '../mongo-db/models';
 import {
-  getToken,
   getTokenFromRequest,
   uploadFile,
   validateSignature,
