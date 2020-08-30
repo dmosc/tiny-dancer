dc63ea4dac7243f6f0492b16d0c6553726abe65e
ernestognw <ernestognw@gmail.com>
2020-08-30 03:15:32 -0500

    whatever

--- server/config/index.js
+++ server/config/index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const {PORT, MONGO_DB_URI, JWT_SECRET} = process.env;
+const {PORT: ENVPORT, MONGO_DB_URI, JWT_SECRET} = process.env;
+
+const PORT = ENVPORT || 4000;
 
 export {PORT, MONGO_DB_URI, JWT_SECRET};
