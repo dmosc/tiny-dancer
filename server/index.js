e8f070aefe5ae6e8fd856d58d5144dc3dcba7700
oscardavidrm <oscardavidrodriguezm@gmail.com>
2020-08-29 21:22:50 -0500

    setup (headers) Add needed headers for the server to allow cross origin requests.

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ import {MONGO_DB_URI, API_PORT} from './config';
 
 const app = express();
 
-app.use((req, res) => res.setHeader('Access-Control-Allow-Origin', '*'));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
