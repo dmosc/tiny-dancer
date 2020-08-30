e9e7f17d2f013a12b69359e43ff200b0f027bd9e
oscardavidrm <oscardavidrodriguezm@gmail.com>
2020-08-29 19:46:31 -0500

    setup (headers) Add headers to backend requests.

--- server/index.js
+++ server/index.js
@@ -2,9 +2,28 @@ import express from 'express';
 import mongoose, {Schema} from 'mongoose';
 import cookieParser from 'cookie-parser';
 import routes from './routes';
-import {MONGO_DB_URI, API_PORT} from './config';
+import {MONGO_DB_URI, API_PORT, CLIENT_ENDPOINT} from './config';
 
 const app = express();
+
+// Add headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', CLIENT_ENDPOINT);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type',
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
