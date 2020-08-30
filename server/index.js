f75eca6ba6d2d3692002569288e6507dac071a6c
oscardavidrm <oscardavidrodriguezm@gmail.com>
2020-08-29 23:47:09 -0500

    bug (yarn.lock) Resolve yarn.lock conflicts.

--- server/index.js
+++ server/index.js
@@ -17,10 +17,6 @@ app.use((req, res, next) => {
     'X-Requested-With,content-type',
   );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', true);
-
   next();
 });
 
